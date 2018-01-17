3fe8bc8443cef3e4ef0d3e1cd6d71be2fa1ab7f3
TAROT TUBE <tarottubeshow@gmail.com>
2018-01-16 19:18:53 -0500

    cleaning up rtmp remnants

--- encode.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-exec ffmpeg -i rtmp://localhost:1935/src/$1\
-    -c:a copy -c:v libx264\
-    -tune:v zerolatency -preset:v ultrafast\
-    -b:v 128k\
-    -vf scale=iw/2:ih/2\
-    -f flv rtmp://localhost:1935/shrink/$1_low
