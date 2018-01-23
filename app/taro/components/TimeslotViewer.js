fb659e50685da916d3126db3408d3dc0e3b12906
TAROT TUBE <tarottubeshow@gmail.com>
2018-01-22 19:27:20 -0500

    android doesnt like svgs, it would appear

--- app/taro/components/TimeslotViewer.js
+++ app/taro/components/TimeslotViewer.js
@@ -169,9 +169,7 @@ const TimeslotViewer = metautil.applyHocs(
 const styles = StyleSheet.create({
   stack: {
     position: 'absolute',
-    bottom: 0,
     left: 0,
-    right: 0,
     top: 0,
     height: '100%',
     width: '100%',
