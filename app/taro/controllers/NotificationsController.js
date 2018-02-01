20ce98c1f34571f2b8e5439c0817d613cf9e4228
TAROT TUBE <tarottubeshow@gmail.com>
2018-02-01 12:48:38 -0500

    always ask

--- app/taro/controllers/NotificationsController.js
+++ app/taro/controllers/NotificationsController.js
@@ -9,41 +9,44 @@ import * as NagManager from 'taro/controllers/NagManager'
 import NOTIFICATIONS_API from 'taro/api/NotificationsApi'
 
 async function maybeRequest() {
-  const { status: existingStatus } = await Permissions.getAsync(
-    Permissions.NOTIFICATIONS
-  )
-  let finalStatus = existingStatus
-
-  if (existingStatus !== 'granted') {
-    const hasSeenRecently = await NagManager.hasSeenRecently(
-      NagManager.ASKED_FOR_NOTIFICATIONS,
-      NagManager.ASKED_FOR_NOTIFICATIONS_CUTOFF,
-    )
-    if(hasSeenRecently) {
-      return
-    }
-
-    await NagManager.setSeen(NagManager.ASKED_FOR_NOTIFICATIONS)
-
-    await TRACKER.track("Asked for notifications permission")
-    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS)
-    if(status == 'granted') {
-      await TRACKER.track("Notifications permission granted")
-    } else {
-      await TRACKER.track("Notifications permission declined")
-    }
-    finalStatus = status
+  const granted = await negotiateStatus()
+  if(granted) {
+    const token = await Notifications.getExpoPushTokenAsync()
+    const response = await NOTIFICATIONS_API.subscribe.exec({
+      token: token,
+    })
   }
-
-  if (finalStatus !== 'granted') {
-    return
-  }
-
-  let token = await Notifications.getExpoPushTokenAsync()
-  const response = await NOTIFICATIONS_API.subscribe.exec({
-    token: token,
-  })
 }
+
+async function doAsk() {
+  await TRACKER.track("Asking for notifications permission")
+  try {
+    const response = await Permissions.askAsync(Permissions.NOTIFICATIONS)
+    return response.status
+  } catch(e) {
+      await TRACKER.track("Exception asking for permissions", {
+        exc: `${ e }`,
+      })
+      return 'error'
+  }
+}
+
+async function negotiateStatus() {
+  // TODO: might want to first check existing status
+
+  const status = await doAsk()
+
+  if(status == 'granted') {
+    await TRACKER.track("Notifications permission granted")
+    return true
+  } else {
+    await TRACKER.track("Notifications permission declined", {
+        status: status,
+    })
+    return false
+  }
+}
+
 
 export {
   maybeRequest,
