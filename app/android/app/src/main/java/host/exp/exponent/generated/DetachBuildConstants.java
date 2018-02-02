03279ad8f4e30c7e5ce9f5c5d8aa2c2c48219407
TAROT TUBE <tarottubeshow@gmail.com>
2018-02-02 16:02:06 -0500

    detached, (failure)

---
+++ app/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp908a9e01a6724b8f8aa3702cf9ff83b3://ux-jx7.kevinjdolan.app.exp.direct:80";
+
+}
