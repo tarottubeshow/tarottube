a76d1577a64bb67fee9920a7c19c12294c0196dc
TAROT TUBE <tarottubeshow@gmail.com>
2018-05-02 19:29:18 -0400

    fix conflicts

--- web/resource/public.js
+++ web/resource/public.js
@@ -2,15 +2,6 @@ $('.full-height').css({
   minHeight: window.innerHeight,
 })
 
-<<<<<<< HEAD
-function completePayment(ev, amount) {
-  fetch('/charge-reading.json', {
-    method: 'POST',
-    body: JSON.stringify({
-      token: ev.token.id,
-      email: ev.payerEmail,
-      name: ev.payerName,
-=======
 function showFail() {
   alert("Something went wrong!")
 }
@@ -25,65 +16,10 @@ function doCheckout(token, amount, cb) {
     method: 'POST',
     body: JSON.stringify({
       token: token,
->>>>>>> origin/master
       amount: amount,
     }),
     headers: {'content-type': 'application/json'},
   })
-<<<<<<< HEAD
-  .then(function(response) {
-    if (response.ok) {
-      ev.complete('success');
-      $('.checkout-buttons').hide()
-      $('.thank-you').show()
-    } else {
-      ev.complete('fail');
-    }
-  })
-}
-
-var paymentRequest15 = window.STRIPE.paymentRequest({
-  country: 'US',
-  currency: 'usd',
-  total: {
-    label: '15-Minute Tarot Reading',
-    amount: 4000,
-  },
-  requestPayerName: true,
-  requestPayerEmail: true,
-})
-
-paymentRequest15.on('token', function(ev) {
-  completePayment(ev, 4000)
-})
-
-var paymentRequest30 = window.STRIPE.paymentRequest({
-  country: 'US',
-  currency: 'usd',
-  total: {
-    label: '30-Minute Tarot Reading',
-    amount: 6000,
-  },
-  requestPayerName: true,
-  requestPayerEmail: true,
-})
-
-paymentRequest30.on('token', function(ev) {
-  completePayment(ev, 6000)
-})
-
-$('.charge-15').click(function() {
-  paymentRequest15.canMakePayment().then(function(result) {
-    paymentRequest15.show()
-  })
-})
-
-$('.charge-30').click(function() {
-  paymentRequest30.canMakePayment().then(function(result) {
-    paymentRequest30.show()
-  })
-})
-=======
   .then(cb)
 }
 
@@ -171,7 +107,6 @@ setupRequest(
   6000,
   '.charge-30'
 )
->>>>>>> origin/master
 
 $('.cta').click(function(ev) {
   const $target = $(ev.currentTarget)
